2709091506f044dfe6f954bde4667156ce9ccf8e
Matin Baloochestani <MatiinBaloochestani@Gmail.com>
2022-10-28 16:31:41 +0330

    Upgrade TwiGram

--- app/config-sample.py
+++ app/config-sample.py
@@ -11,15 +11,12 @@ MONGO_URI = f"mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@"\
             f"{MONGO_HOST}:{MONGO_PORT}/my-app?authSource=admin"
 
 
-BOT_USERNAME = ""
+BOT_USERNAME = "TwiDLBot"
 BOT_URL = f"https://t.me/{BOT_USERNAME}"
 CHANNEL_URL = "https://t.me/"
-
-PRIVATE_CHANNEL_DB = int("") # Sample: -100123456789
-PUBLIC_CHANNEL_DB = int("")
+CHANNEL_USERNAME = "TwiDLChannel"
 
 
 ADMIN_LIST = [
     711692445, # Matin
-    258872582, # Amir
 ]
